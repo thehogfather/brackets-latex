671c61c29609c8a47fe00ffbacc9a72870794d7a
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2020-04-22 14:35:37 -0500

    Update main.js

--- main.js
+++ main.js
@@ -174,7 +174,7 @@ define(function (require, exports, module) {
             }
         });
 
-        LanguageManager.defineLanguage("Latex", {
+        LanguageManager.defineLanguage("latex", {
             name: "LaTeX",
             mode: ["stex", "text/x-stex"],
             fileExtensions: ["tex", "bib", "cls", "ltx", "clo", "sty", "def"],
